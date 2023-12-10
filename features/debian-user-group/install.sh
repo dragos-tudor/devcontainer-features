2bf2f5b95bb54e96431afa11653668722adbdc66
Dragos Tudor <dragos.tudor@gmail.com>
2023-12-10 15:08:28 +0200

    feat: unsupport root user.

--- features/debian-user-group/install.sh
+++ features/debian-user-group/install.sh
@@ -1,7 +1,11 @@
 set -e
+
+[ $_REMOTE_USER = root ] && echo "Root user not supported" && exit 1;
+
 USER=${_REMOTE_USER:-vscode}
 GROUP=${_REMOTE_USER:-vscode}
 USER_HOME=/home/$USER
+
 groupadd -g 1000 $GROUP &&
 	groupadd -g 999 docker &&
   useradd -m -d $USER_HOME -s /bin/bash -g 1000 -G 999 -u 1000 $USER
