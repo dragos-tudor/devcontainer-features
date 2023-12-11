3c8f4246b00c1754901b66ee14a9d357fcd371c4
Dragos Tudor <dragos.tudor@gmail.com>
2023-12-11 16:13:19 +0200

    feat: add uid and gid debian-user-group options.

--- features/debian-user-group/install.sh
+++ features/debian-user-group/install.sh
@@ -5,10 +5,12 @@ set -e
 USER=${_REMOTE_USER:-vscode}
 GROUP=${_REMOTE_USER:-vscode}
 USER_HOME=/home/$USER
+UID=${UID:-1000}
+GID=${GID:-1000}
 
-groupadd -g 1000 $GROUP &&
+groupadd -g $GID $GROUP &&
 	groupadd -g 999 docker &&
-  useradd -m -d $USER_HOME -s /bin/bash -g 1000 -G 999 -u 1000 $USER
+  useradd -m -d $USER_HOME -s /bin/bash -g $GID -G 999 -u $UID $USER
 
-echo "Devcontainer feature [debian-user-group]: User $USER created."
-echo "Devcontainer feature [debian-user-group]: Group $GROUP created."  
+echo "Devcontainer feature [debian-user-group]: User $USER:$UID created."
+echo "Devcontainer feature [debian-user-group]: Group $GROUP:$GID created."  
