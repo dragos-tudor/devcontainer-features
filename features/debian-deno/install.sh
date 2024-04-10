b94c990abe15d351219d884c9cd2db68d94f0fa9
Dragos Tudor <dragos.tudor@gmail.com>
2024-04-10 16:45:53 +0300

    change(debian-deno): add feature options (os, arch).

--- features/debian-deno/install.sh
+++ features/debian-deno/install.sh
@@ -3,8 +3,11 @@ set -e
 USER=${_REMOTE_USER:-vscode}
 USER_HOME=/home/$USER
 DENO_ROOT=$USER_HOME/.deno
+DENO_RELEASE_URL="https://github.com/denoland/deno/releases/download/$VERSION/deno-$ARCH-$OS.zip"
 
-curl -fsSL https://github.com/denoland/deno/releases/download/$DENO_VERSION/deno-x86_64-unknown-linux-gnu.zip -o $USER_HOME/deno.zip &&
+echo "Devcontainer feature [debian-deno]: install deno from: $DENO_RELEASE_URL"
+
+curl -fsSL $DENO_RELEASE_URL -o $USER_HOME/deno.zip &&
     unzip -d $DENO_ROOT -o $USER_HOME/deno.zip &&
     chmod +x $DENO_ROOT/deno &&
     chown -R $USER:$USER $DENO_ROOT &&
@@ -19,4 +22,4 @@ export DENO_DIR=$DENO_CACHE
 export PATH="\$PATH:$DENO_ROOT"
 EOF
 
-echo "Devcontainer feature [debian-deno]: deno installed, deno root: $DENO_ROOT, deno cache: $DENO_CACHE, deno version: $DENO_VERSION"
+echo "Devcontainer feature [debian-deno]: deno installed, deno root: $DENO_ROOT, deno cache: $DENO_CACHE, deno version: $VERSION"
