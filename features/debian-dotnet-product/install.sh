7448fa6a2b13c1a4593c40d74a4701f5b9f29dd8
Dragos Tudor <dragos.tudor@gmail.com>
2024-07-05 17:08:17 +0300

    fix(dotnet): correct dotnet product url.

--- features/debian-dotnet-product/install.sh
+++ features/debian-dotnet-product/install.sh
@@ -6,9 +6,9 @@ USER_HOME=/home/$USER
 DOTNET_PATH=$USER_HOME/.dotnet
 DOTNET_ROOT=$DOTNET_PATH
 
-if [ "$PRODUCT" == "sdk" ]; then DOTNET_PRODUCT_URL="https://dotnetcli.azureedge.net/dotnet/sdk/$VERSION/dotnet-sdk-$VERSION-${OS}-${ARCH}.${EXT}"; fi
-if [ "$PRODUCT" == "aspnetcore" ]; then DOTNET_PRODUCT_URL="https://dotnetcli.azureedge.net/dotnet/aspnetcore/runtime/$VERSION/aspnetcore-runtime-$VERSION-${OS}-${ARCH}.${EXT}"; fi
-if [ "$PRODUCT" == "runtime" ]; then DOTNET_PRODUCT_URL="https://dotnetcli.azureedge.net/dotnet/runtime/$VERSION/dotnet-runtime-$VERSION-${OS}-${ARCH}.${EXT}"; fi
+if [ "$PRODUCT" == "sdk" ]; then DOTNET_PRODUCT_URL="https://dotnetcli.azureedge.net/dotnet/Sdk/$VERSION/dotnet-sdk-$VERSION-${OS}-${ARCH}.${EXT}"; fi
+if [ "$PRODUCT" == "aspnetcore" ]; then DOTNET_PRODUCT_URL="https://dotnetcli.azureedge.net/dotnet/aspnetcore/Runtime/$VERSION/aspnetcore-runtime-$VERSION-${OS}-${ARCH}.${EXT}"; fi
+if [ "$PRODUCT" == "runtime" ]; then DOTNET_PRODUCT_URL="https://dotnetcli.azureedge.net/dotnet/Runtime/$VERSION/dotnet-runtime-$VERSION-${OS}-${ARCH}.${EXT}"; fi
 
 cat << EOF >> /.devcontainer_features.log
 Devcontainer feature [debian-dotnet-product]: install dotnet product from: $DOTNET_PRODUCT_URL.
